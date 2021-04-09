b61fb028d0281bc0e9a354252b39488ee9c73080
rynekolessar <ryne.kolessar@gmail.com>
2021-04-09 12:58:54 -0400

    initial commit

---
+++ HotelBooking/src/main/java/com/Motel6/HotelBooking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.Motel6.HotelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
---
+++ HotelBooking/src/test/java/com/Motel6/HotelBooking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Motel6.HotelBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
