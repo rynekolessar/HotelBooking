b8ddc65ac69b653b69fc7f449aa2edc4503c1fa2
Louis_VT <louis9286@icloud.com>
2021-04-16 21:32:00 -0500

    commit on 'master'

---
+++ HotelBooking/src/main/java/com/Motel6/HotelBooking/Main.java
@@ -0,0 +1,12 @@
+package edu.depaul.cdm.se452.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+}
