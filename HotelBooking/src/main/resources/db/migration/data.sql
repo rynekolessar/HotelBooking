dae553a45d92bb9ed80ee84289941fd9aa0e388d
jsciacco <jsciacco@depaul.edu>
2021-04-19 15:13:12 -0500

    data and schema sql

---
+++ HotelBooking/src/main/resources/db/migration/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO hotels(id, roomNum, citySt, roomType) VALUES
+    ('101', 2, 'Chicago, IL', 'Suite'),
+    ('102', 5, 'Chicago, IL', 'King'),
+    ('103', 10, 'Chicago, IL', 'Queen'),
+    ('201', 2, 'Los Angeles, CA', 'Suite'),
+    ('202', 5, 'Los Angeles, CA', 'King'),
+    ('203', 10, 'Los Angeles, CA', 'Queen'),
+    ('301', 2, 'New York, NY', 'Suite'),
+    ('302', 5, 'New York, NY', 'King'),
+    ('303', 10, 'New York, NY', 'Queen');
+
+INSERT INTO rooms(id, roomType, cost) VALUES
+    ('001', 'Suite', 200.00),
+    ('002', 'King', 100.00),
+    ('003', 'Queen', 50.00);
+
+    
---
+++ HotelBooking/src/main/resources/db/migration/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS hotels;
+DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS guests;
+
+create table hotels (
+    id serial primary key,
+    roomNum int,
+    citySt varchar(50),
+    roomType varchar(50),
+);
+
+create table rooms (
+    id serial primary key,
+    roomType varchar(50),
+    cost decimal (10,2),
+);
+
+create table guests (
+    if INT AUTO_INCREMENT PRIMARY KEY.
+    guest_id VARCHAR(10),
+    name VARCHAR(50),
+    email VARCHAR(50),
+    admitted_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (ID)
+);
+
+    
