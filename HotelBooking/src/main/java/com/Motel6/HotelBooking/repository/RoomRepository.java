2150444b39509fe689efa6efb8230fabc0fb3ccc
jsciacco <jsciacco@depaul.edu>
2021-05-26 22:20:36 -0500

    room classes

---
+++ HotelBooking/src/main/java/com/Motel6/HotelBooking/repository/RoomRepository.java
@@ -0,0 +1,31 @@
+package com.Motel6.HotelBooking.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.Motel6.HotelBooking.model.Motel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+public class RoomRepository {
+
+    @Autowired Room roomModel;
+
+    @Autowired JdbcTemplate template;
+
+    public List<Room> getAll() {
+        String sql = "select * from rooms";
+        RowMapper<Room> rm = new RowMapper<Room>() {
+            @Override
+            public Motel mapRow(ResultSet resultSet, int i) throws SQLException{
+                Room room = new Room(resultSet.getString("roomType"), resultSet.getInt("roomcode"), resultSet.getFloat("cost"));
+                return room;
+            }
+        };
+        return template.query(sql, rm);
+    }
+    
+}
